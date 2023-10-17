0fc4a1b006ed03835235c77fd9547a1c1786598e
Raúl Corchero <corchero100@gmail.com>
2023-10-17 21:53:38 +0100

    red: Show_love_love_when_is_a_new_game

---
+++ TennisKata/TennisKata.Tests/GameGiven.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+
+namespace TennisKata.Tests {
+    public class GameGiven {
+
+        [SetUp]
+        public void Setup() {
+           
+        }
+
+        [Test]
+        public void Show_love_love_when_is_a_new_game()
+        {
+            var result = new Game().Result;
+
+            result.Should().Be("Player1:love-Player2:love");
+        }
+    }
+
+    public class Game
+    {
+        public string Result { get; set; }
+    }
+}
